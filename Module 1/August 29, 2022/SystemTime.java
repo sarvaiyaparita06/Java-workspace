bfed7c0e7fa1873b90b88ec4b1da8586d6411010
sarvaiyaparita06 <111833490+sarvaiyaparita06@users.noreply.github.com>
2022-09-08 14:54:48 -0500

    Delete Module 1/August 29, 2022 directory

--- Module 1/August 29, 2022/SystemTime.java
+++
@@ -1,9 +0,0 @@
-package August302022;
-
-public class SystemTime 
-{
-	public static void main(String[] args){
-        System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
-   }
-
-}
