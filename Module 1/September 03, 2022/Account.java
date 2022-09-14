3519182c4c8f2c63fb6fc762c1dc1048f98ee2c9
sarvaiyaparita06 <111833490+sarvaiyaparita06@users.noreply.github.com>
2022-09-14 15:29:11 -0500

    Create Account.java

---
+++ Module 1/September 03, 2022/Account.java
@@ -0,0 +1,23 @@
+package September032022;
+
+public class Account 
+{
+ 
+    private int balance = 2000;
+ 
+    public int balance() 
+    {
+        return balance;
+    }
+ 
+    public void withdraw(int amount) throws InsufficientFunds 
+    {
+        if (amount > balance) 
+        {
+            throw new InsufficientFunds(
+   String.format("Sorry, insufficient funds. You need more 500 to complete this transaction."));
+        }
+        balance = balance - amount;
+    }
+ 
+}
